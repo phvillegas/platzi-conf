b0061e97ed2094090ae1c14b4afcd5ee0e1659b8
Pedro Hernández <phvillegas@gmail.com>
2021-03-20 17:50:11 -0300

    app desing. Fragments, Items and Activity

---
+++ app/src/main/java/com/platzi/conf/ui/activities/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.platzi.conf.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.platzi.conf.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
